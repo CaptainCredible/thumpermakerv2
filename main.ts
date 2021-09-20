cfbc11fc3752c4b341fd092f00df852a7cf50347
CaptainCredible <cpt.credible@gmail.com>
2021-09-20 12:20:51 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+let thumperNr = 9
+let rabbitNames = ["Ali", "Eir", "Ina", "Una", "Per", "Alf", "Ada", "Ela", "Eli", "Mor", "Oda", "Ask", "Kai", "Ida", "Kim", "Eva", "Bob"]
+music.playTone(440, 50)
+music.playTone(880, 50)
+music.playTone(1760, 50)
+OrchestraInstrument.makeAThumper(rabbitNames[thumperNr], thumperType.thumpBit)
+OrchestraInstrument.slaveToBun(thumperNr, bunSolactions.tok)
